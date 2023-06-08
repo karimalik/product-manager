98f279e7aad4c4f29c670d843609d0fa838cfe62
karimalik <me@karimdev.network>
2023-06-08 22:05:10 +0200

    create dto user and Api tags in users constroller

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { User } from "../entities/user.entity";
+
+/**
+ * user DTO
+ * @author karim kompissi <karimkompissi@gmail.com>
+ */
+export class CreateUserDto extends User {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 
 /**
  * user entity
  * @author karim kompissi <karimkompissi@gmail.com>
  */
 export class User {
+    @ApiProperty()
     name: string;
+
+    @ApiProperty()
     email: string;
+
+    @ApiProperty()
     password: string;
 }
