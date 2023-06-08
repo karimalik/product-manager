ad77262c748e209f3afcc4dd0ffd01ed504a73e9
karimalik <me@karimdev.network>
2023-06-08 18:53:28 +0200

    configuration of swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //swagger config
+  const config = new DocumentBuilder()
+        .setTitle('Product Manager')
+        .setDescription('API product manager')
+        .setVersion('1.0')
+        .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, doc);
+
   await app.listen(3000);
 }
 bootstrap();
