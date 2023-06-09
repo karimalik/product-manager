8c31660d2cdb7d228f53de99b94f7a1676fceff2
karimalik <me@karimdev.network>
2023-06-09 15:57:19 +0200

    modify

--- src/users/hash.service.ts
+++ src/users/hash.service.ts
@@ -8,14 +8,14 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class HashService {
-    //hash password
-    // async hashPassword(password: string) {
-    //     const salt = 10;
-    //     return await bcrypt.hash(password, salt);
-    // }
+   // hash password
+    async hashPassword(password: string) {
+        const saltOrRouds = 10;
+        return await bcrypt.hash(password, saltOrRouds);
+    }
 
-    // //compare password
-    // async comparePassword(password: string, hash) {
-    //     return await bcrypt.compare(password, hash);
-    // }
+    //compare password
+    async comparePassword(password: string, hash) {
+        return await bcrypt.compare(password, hash);
+    }
 }
