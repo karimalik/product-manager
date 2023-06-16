e0a9c4ebb92a76fe149e88e7677aff39593cbbb7
karimalik <me@karimdev.network>
2023-06-16 07:38:19 +0200

    product schema

--- src/product/schemas/product.schema.ts
+++ src/product/schemas/product.schema.ts
@@ -1,13 +1,38 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Date, ObjectId } from "mongoose";
+import * as mongoose from 'mongoose';
+import { Fournisseur } from "src/fournisseur/schemas/fournisseur.schema";
+import { Transform, Type } from 'class-transformer';
 
 
 export type ProductDocument = Product & Document;
 
 @Schema()
 export class Product {
+    @Transform(({ value }) => value.toString())
+    _id: ObjectId;
+
     @Prop({
         required: true,
         type: String
     })
     name: string;
+
+    @Prop({
+        required: true,
+        type: Date
+    })
+    dateDelivrance: Date;
+
+    @Prop({
+        required: true,
+        type: Date
+    })
+    dateExpriration: Date;
+
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Fournisseur.name })
+    @Type(() => Fournisseur)
+    fournisseur: Fournisseur;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
