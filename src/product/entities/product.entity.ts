c3beca236f390c35b6ca0833769e3288f906a370
karimalik <me@karimdev.network>
2023-06-16 07:43:51 +0200

    product dto and  entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsDate, IsNotEmpty, IsString } from "class-validator";
+
+export class Product {
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty()
+    @IsDate()
+    @IsNotEmpty()
+    dateDelivrance: Date;
+
+    @ApiProperty()
+    @IsDate()
+    @IsNotEmpty()
+    dateExpiration: Date;
+}
