88b9f507274d0365b6b4581621bc5763c4feece2
karimalik <me@karimdev.network>
2023-06-16 07:58:29 +0200

    modified

--- src/product/schemas/product.schema.ts
+++ src/product/schemas/product.schema.ts
@@ -1,38 +1,37 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Date, ObjectId } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Date, ObjectId } from 'mongoose';
 import * as mongoose from 'mongoose';
 import { Fournisseur } from '../../fournisseur/schemas/fournisseur.schema';
 import { Transform, Type } from 'class-transformer';
-
 
 export type ProductDocument = Product & Document;
 
 @Schema()
 export class Product {
-    @Transform(({ value }) => value.toString())
-    _id: ObjectId;
+  @Transform(({ value }) => value.toString())
+  _id: ObjectId;
 
-    @Prop({
-        required: true,
-        type: String
-    })
-    name: string;
+  @Prop({
+    required: true,
+    type: String,
+  })
+  name: string;
 
-    @Prop({
-        required: true,
-        type: Date
-    })
-    dateDelivrance: Date;
+  @Prop({
+    required: true,
+    type: Date,
+  })
+  dateDelivrance: Date;
 
-    @Prop({
-        required: true,
-        type: Date
-    })
-    dateExpriration: Date;
+  @Prop({
+    required: true,
+    type: Date,
+  })
+  dateExpriration: Date;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Fournisseur.name })
-    @Type(() => Fournisseur)
-    fournisseur: Fournisseur;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Fournisseur.name })
+  @Type(() => Fournisseur)
+  fournisseur: Fournisseur;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
